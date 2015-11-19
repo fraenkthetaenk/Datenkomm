be234178ce07f222bdbad832f24c5e5cab114111
fraenkthetaenk <steinefr@MUCNCNU4319PPF.eu.infineon.com>
2015-11-19 08:46:54 +0100

    Create Project
    Erstellen des Projectes

---
+++ src/main/java/MAIN.java
@@ -0,0 +1,16 @@
+import edu.hm.dako.chat.client.ChatClientSwingGUI;
+import edu.hm.dako.chat.server.ServerFactory;
+
+
+public class MAIN {
+
+	public static void main(String[] args) throws Exception {
+		ChatClientSwingGUI.main(args);
+		ChatClientSwingGUI.main(args);
+		ServerFactory.main(args);
+		
+		
+
+	}
+
+}
